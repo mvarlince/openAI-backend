45bc72749fdb3dc56565fce8962cb4dce8127a5d
Varlince Meran <varlincemeran@gmail.com>
2023-05-06 15:44:37 -0400

    changes to promp

--- functions/src/utils.js
+++ functions/src/utils.js
@@ -1,29 +1,26 @@
-import { Configuration, OpenAIApi } from "openai"
-import { apiKey } from "./secrets.js"
+import { Configuration, OpenAIApi } from "openai";
+import { apiKey } from "./secrets.js";
 
 const configuration = new Configuration({
   apiKey: apiKey,
-})
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
-const generatePrompt = (text) => {
-  return `return the sentiment analysis of: ${text}.
-    
-    text: I am very happy.
-    analysis: Positive`
-}
+// const generatePrompt = (text) => {
+//   return `return the sentiment analysis of: ${text}.`;
+// };
 
 export const getAnalysis = async (req, res) => {
   const {userInput} = req.body
   console.log(`userInput: ${userInput}`)
 
   const response = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: generatePrompt(userInput),
+    model: "text-davinci-003",
+    // prompt: generatePrompt(userInput),
+    prompt: `return postive, negative or neutral sentiment analysis of: ${userInput}`,
     temperature: 1,
-  })
-//   const sentiment = response.data
-  res.send(response.data)
-  console.log(`the sentiment result is ${response.choices}`)
-}
+  });
+  //   const sentiment = response.data
+  res.send(response.data);
+};
